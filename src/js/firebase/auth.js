82ed41a97a69dfe1038cb220a04740383fba9a6e
Enrique Munoz <enriquemunozdev@gmail.com>
2023-02-01 20:09:19 -0600

    Basic setup for firebase old fashioned way

--- src/js/firebase/auth.js
+++ src/js/firebase/auth.js
@@ -1,15 +1,3 @@
-
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDwAv4dPHvXBGTXWk9N4lemF9P6vPhmEXQ",
-    authDomain: "paisajismo-munoza.firebaseapp.com",
-    databaseURL: "https://paisajismo-munoza-default-rtdb.firebaseio.com",
-    projectId: "paisajismo-munoza",
-    storageBucket: "paisajismo-munoza.appspot.com",
-    messagingSenderId: "1013941730612",
-    appId: "1:1013941730612:web:c1d3e7c6ded8f33d30f961",
-    measurementId: "G-0B66K64DG6"
-});
-
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 
